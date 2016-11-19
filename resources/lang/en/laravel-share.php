8a846e117858fd878ec782eac922c012844ee15d
Van Hocht, Joren <Joren.VanHocht@thesedays.com>
2016-11-19 19:20:21 +0100

    Cleanup Share class & fix styleci errors

--- resources/lang/en/laravel-share.php
+++ resources/lang/en/laravel-share.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'facebook' => '<a href=":url" class="social-button :class" id=":id"><span class="fa fa-facebook-official"></span></a>',
-    'twitter' => '<a href=":url" class="social-button :class" id=":id"><span class="fa fa-twitter"></span></a>',
-    'gplus' => '<a href=":url" class="social-button :class" id=":id"><span class="fa fa-google-plus"></span></a>',
-    'linkedin' => '<a href=":url" class="social-button :class" id=":id"><span class="fa fa-linkedin"></span></a>',
+    'facebook' => '<li><a href=":url" class="social-button :class" id=":id"><span class="fa fa-facebook-official"></span></a></li>',
+    'twitter' => '<li><a href=":url" class="social-button :class" id=":id"><span class="fa fa-twitter"></span></a></li>',
+    'gplus' => '<li><a href=":url" class="social-button :class" id=":id"><span class="fa fa-google-plus"></span></a></li>',
+    'linkedin' => '<li><a href=":url" class="social-button :class" id=":id"><span class="fa fa-linkedin"></span></a></li>',
 ];
--- src/Share.php
+++ src/Share.php
@@ -4,77 +4,175 @@ namespace Jorenvh\Share;
 
 class Share
 {
-
+    /**
+     * The url of the page to share
+     *
+     * @var string
+     */
     protected $url;
 
+    /**
+     * Optional text for twitter
+     * and title for Linkedin
+     *
+     * @var string
+     */
     protected $title;
 
+    /**
+     * Extra options for the share links
+     *
+     * @var array
+     */
     protected $options = [];
 
+    /**
+     * Html to prefix before the share links
+     *
+     * @var string
+     */
+    protected $prefix = '<div id="social-links"></ul>';
+
+    /**
+     * Html to append after the share links
+     *
+     * @var string
+     */
+    protected $suffix = '</ul></div>';
+
+    /**
+     * The generated html
+     *
+     * @var string
+     */
     protected $html = '';
 
-    public function page($url, $title = null, $options = [])
+    /**
+     * Return a string with html at the end
+     * of the chain.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        $this->html = $this->prefix . $this->html;
+        $this->html .= $this->suffix;
+
+        return $this->html;
+    }
+
+    /**
+     * @param $url
+     * @param null $title
+     * @param array $options
+     * @param null $prefix
+     * @param null $suffix
+     * @return $this
+     */
+    public function page($url, $title = null, $options = [], $prefix = null, $suffix = null)
     {
         $this->url = $url;
         $this->title = $title;
         $this->options = $options;
 
+        $this->setPrefixAndSuffix($prefix, $suffix);
+
         return $this;
     }
 
+    /**
+     * Facebook share link
+     *
+     * @return $this
+     */
     public function facebook()
     {
-        $base = config('laravel-share.services.facebook.uri');
-        $this->html .= trans('laravel-share::laravel-share.facebook', [
-            'url' => $base . $this->url,
-            'class' => key_exists('class', $this->options) ? $this->options['class'] : '',
-            'id' => key_exists('id', $this->options) ? $this->options['id'] : '',
-        ]);
+        $url = config('laravel-share.services.facebook.uri') . $this->url;
 
-        return $this->html;
+        $this->buildLink('facebook', $url);
+
+        return $this;
     }
 
+    /**
+     * Twitter share link
+     *
+     * @return $this
+     */
     public function twitter()
     {
-        if(is_null($this->title)) {
+        if (is_null($this->title)) {
             $this->title = config('laravel-share.services.twitter.text');
         }
-        $base = config('laravel-share.services.twitter.uri');
-        $this->html .= trans('laravel-share::laravel-share.twitter', [
-            'url' => $base . '?text=' . $this->title . '&url=' . $this->url,
-            'class' => key_exists('class', $this->options) ? $this->options['class'] : '',
-            'id' => key_exists('id', $this->options) ? $this->options['id'] : '',
-        ]);
 
-        return $this->html;
+        $base = config('laravel-share.services.twitter.uri');
+        $url = $base . '?text=' . $this->title . '&url=' . $this->url;
+
+        $this->buildLink('twitter', $url);
+
+        return $this;
     }
 
+    /**
+     * Google Plus share link
+     *
+     * @return $this
+     */
     public function googlePlus()
     {
-        $base = config('laravel-share.services.gplus.uri');
-        $this->html .= trans('laravel-share::laravel-share.gplus', [
-            'url' => $base . $this->url,
-            'class' => key_exists('class', $this->options) ? $this->options['class'] : '',
-            'id' => key_exists('id', $this->options) ? $this->options['id'] : '',
-        ]);
+        $url = config('laravel-share.services.gplus.uri') . $this->url;
 
-        return $this->html;
+        $this->buildLink('gplus', $url);
+
+        return $this;
     }
 
+    /**
+     * Linked in share link
+     *
+     * @param string $summary
+     * @return $this
+     */
     public function linkedin($summary = '')
     {
         $base = config('laravel-share.services.linkedin.uri');
-        $this->html .= trans('laravel-share::laravel-share.linkedin', [
-            'url' => $base . '?mini='.config('laravel-share.services.linkedin.extra.mini').'&url=' . $this->url . '&title=' . $this->title . '&summary=' . $summary,
+        $mini = config('laravel-share.services.linkedin.extra.mini');
+        $url = $base . '?mini=' . $mini . '&url=' . $this->url . '&title=' . $this->title . '&summary=' . $summary;
+
+        $this->buildLink('linkedin', $url);
+
+        return $this;
+    }
+
+    /**
+     * Build a single link
+     *
+     * @param $provider
+     * @param $url
+     */
+    protected function buildLink($provider, $url)
+    {
+        $this->html .= trans("laravel-share::laravel-share.$provider", [
+            'url' => $url,
             'class' => key_exists('class', $this->options) ? $this->options['class'] : '',
             'id' => key_exists('id', $this->options) ? $this->options['id'] : '',
         ]);
-
-        return $this->html;
     }
 
-    public function email()
+    /**
+     * Optionally Set custom prefix and/or suffix
+     *
+     * @param $prefix
+     * @param $suffix
+     */
+    protected function setPrefixAndSuffix($prefix, $suffix)
     {
-        //
+        if (!is_null($prefix)) {
+            $this->prefix = $prefix;
+        }
+
+        if (!is_null($suffix)) {
+            $this->suffix = $suffix;
+        }
     }
 }
