4d0134dc0c76d3cee48e727b608aae9fbbc48bfe
Charlie Langridge <charlie@charlielangridge.com>
2018-01-12 17:48:26 +0000

    Add FontAwesome 5 Compatibility
    Add new config value for fontawesome version and point at new translation for fa4
    
    Rename (as actually FA5!)
    
    Update Share.php to fa5
    
    Adjust Readme File
    
    Default FA is 4, to allow tests to pass
    
    allow for old config file

---
+++ resources/lang/en/laravel-share-fa5.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'facebook' => '<li><a href=":url" class="social-button :class" id=":id"><i class="fab fa-facebook"></i></a></li>',
+    'twitter' => '<li><a href=":url" class="social-button :class" id=":id"><i class="fab fa-twitter"></i></a></li>',
+    'gplus' => '<li><a href=":url" class="social-button :class" id=":id"><i class="fab fa-google-plus-g"></i><a></li>',
+    'linkedin' => '<li><a href=":url" class="social-button :class" id=":id"><i class="fab fa-linkedin"></i></span></a></li>',
+];
+ 
