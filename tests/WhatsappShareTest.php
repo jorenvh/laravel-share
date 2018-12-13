d0cfe9a9e55f5c6eab965134e49b080621c60c24
alvaro <pqrs@pqrs.es>
2018-12-13 11:59:35 +0100

    Update WhatsappShareTest.php

--- tests/WhatsappShareTest.php
+++ tests/WhatsappShareTest.php
@@ -32,14 +32,53 @@ class WhatsappShareTest extends TestCase
     /**
      * @test
      */
-    public function it_can_generate_a_whatsapp_share_link_with_custom_share_text()
+    public function it_can_generate_a_whatsapp_share_link_with_a_custom_class()
     {
-        $result = ShareFacade::page('https://codeswitch.be', 'Meet Joren Van Hocht a php developer with a passion for laravel')
+        $result = ShareFacade::page('https://codeswitch.be', , ['class' => 'my-class'])
             ->whatsapp();
-        $expected = '<div id="social-links"><ul><li><a href="https://twitter.com/intent/tweet?text=Meet+Joren+Van+Hocht+a+php+developer+with+a+passion+for+laravel&url=https://codeswitch.be" class="social-button " id=""><span class="fa fa-twitter"></span></a></li></ul></div>';
+        $expected = '<div id="social-links"><ul><li><a target="_blank" href="https://wa.me/?text=https://codeswitch.be" class="social-button my-class" id=""><span class="fa fa-whatsapp"></span></a></li></ul></div>';
 
         $this->assertEquals($expected, $result);
     }
+
+    /**
+     * @test
+     */
+    public function it_can_generate_a_whatsapp_share_link_with_a_custom_class_with_fa5()
+    {
+        config(['laravel-share.fontAwesomeVersion' => 5]);
+        $result = ShareFacade::page('https://codeswitch.be', , 'Meet Joren Van Hocht a php developer with a passion for laravel', ['class' => 'my-class'])
+            ->whatsapp();
+        $expected = '<div id="social-links"><ul><li><a target="_blank" href="https://wa.me/?text=https://codeswitch.be" class="social-button my-class" id=""><span class="fab fa-whatsapp"></span></a></li></ul></div>';
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_generate_a_whatsapp_share_link_with_a_custom_id()
+    {
+        $result = ShareFacade::page('https://codeswitch.be', , ['id' => 'my-id'])
+            ->twitter();
+        $expected = '<div id="social-links"><ul><li><a target="_blank" href="https://wa.me/?text=https://codeswitch.be" class="social-button " id="my-id"><span class="fa fa-whatsapp"></span></a></li></ul></div>';
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_generate_a_whatsapp_share_link_with_a_custom_id_with_fa5()
+    {
+        config(['laravel-share.fontAwesomeVersion' => 5]);
+        $result = ShareFacade::page('https://codeswitch.be', 'Meet Joren Van Hocht a php developer with a passion for laravel', ['id' => 'my-id'])
+            ->twitter();
+        $expected = '<div id="social-links"><ul><li><a target="_blank" href="https://wa.me/?text=https://codeswitch.be" class="social-button " id="my-id"><span class="fab fa-whatsapp"></span></a></li></ul></div>';
+
+        $this->assertEquals($expected, $result);
+    }
+
     
 
 }
