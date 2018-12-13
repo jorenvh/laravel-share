4bc783f9aad0ecfe0d1790db0f0977e5e56036ce
alvaro <pqrs@pqrs.es>
2018-12-13 12:10:47 +0100

    Update WhatsappShareTest.php

--- tests/WhatsappShareTest.php
+++ tests/WhatsappShareTest.php
@@ -47,7 +47,7 @@ class WhatsappShareTest extends TestCase
     public function it_can_generate_a_whatsapp_share_link_with_a_custom_class_with_fa5()
     {
         config(['laravel-share.fontAwesomeVersion' => 5]);
-        $result = ShareFacade::page('https://codeswitch.be', null , 'Meet Joren Van Hocht a php developer with a passion for laravel', ['class' => 'my-class'])
+        $result = ShareFacade::page('https://codeswitch.be', null, ['class' => 'my-class'])
             ->whatsapp();
         $expected = '<div id="social-links"><ul><li><a target="_blank" href="https://wa.me/?text=https://codeswitch.be" class="social-button my-class" id=""><span class="fab fa-whatsapp"></span></a></li></ul></div>';
 
@@ -59,7 +59,7 @@ class WhatsappShareTest extends TestCase
      */
     public function it_can_generate_a_whatsapp_share_link_with_a_custom_id()
     {
-        $result = ShareFacade::page('https://codeswitch.be', null , ['id' => 'my-id'])
+        $result = ShareFacade::page('https://codeswitch.be', null, ['id' => 'my-id'])
             ->whatsapp();
         $expected = '<div id="social-links"><ul><li><a target="_blank" href="https://wa.me/?text=https://codeswitch.be" class="social-button " id="my-id"><span class="fa fa-whatsapp"></span></a></li></ul></div>';
 
