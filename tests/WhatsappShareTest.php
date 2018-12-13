38c87dfa5e5865bafcc8bf4cc8d9398da4cdf84f
alvaro <pqrs@pqrs.es>
2018-12-13 11:12:03 +0100

    Whatsapp test

---
+++ tests/WhatsappShareTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Jorenvh\Share\Test;
+
+use Jorenvh\Share\ShareFacade;
+
+class WhatsappShareTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_can_generate_a_whatsapp_share_link_with_default_share_text()
+    {
+        $result = ShareFacade::page('https://codeswitch.be')->whatsapp();
+        $expected = '<div id="social-links"><ul><li><a target="_blank" href="https://wa.me/?text=https://codeswitch.be" id=""><span class="fa fa-whatsapp"></span></a></li></ul></div>';
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_generate_a_whatsapp_share_link_with_default_share_text_with_fa5()
+    {
+        config(['laravel-share.fontAwesomeVersion' => 5]);
+        $result = ShareFacade::page('https://codeswitch.be')->whatsapp();
+        $expected = '<div id="social-links"><ul><li><a target="_blank" href="https://wa.me/?text=https://codeswitch.be" id=""><span class="fab fa-whatsapp"></span></a></li></ul></div>';
+
+        $this->assertEquals($expected, $result);
+    }
+
+}
