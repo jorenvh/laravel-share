889b300baaa9273b534c4dcc57921f0ba291ca9f
Danish Saleem <danishsaleem167@gmail.com>
2019-05-16 14:41:19 +0500

    fixes

--- resources/lang/en/laravel-share-fa4.php
+++ resources/lang/en/laravel-share-fa4.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'facebook' => '<li><a href=":url" class="social-button :class" id=":id"><span class="fa fa-facebook-official"></span></a></li>',
-    'twitter' => '<li><a href=":url" class="social-button :class" id=":id"><span class="fa fa-twitter"></span></a></li>',
-    'gplus' => '<li><a href=":url" class="social-button :class" id=":id"><span class="fa fa-google-plus"></span></a></li>',
-    'linkedin' => '<li><a href=":url" class="social-button :class" id=":id"><span class="fa fa-linkedin"></span></a></li>',
-    'whatsapp' => '<li><a target="_blank" href=":url" class="social-button :class" id=":id"><span class="fa fa-whatsapp"></span></a></li>',
+    'facebook' => '<a href=":url" class="social-button :class" id=":id"></a>',
+    'twitter' => '<a href=":url" class="social-button :class" id=":id"></a>',
+    'gplus' => '<a href=":url" class="social-button :class" id=":id"></a>',
+    'linkedin' => '<a href=":url" class="social-button :class" id=":id"></a>',
+    'whatsapp' => '<a target="_blank" href=":url" class="social-button :class" id=":id"></a>',
 ];
  
--- resources/lang/en/laravel-share-fa5.php
+++ resources/lang/en/laravel-share-fa5.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'facebook' => '<li><a href=":url" class="social-button :class" id=":id"><span class="fab fa-facebook-square"></span></a></li>',
-    'twitter' => '<li><a href=":url" class="social-button :class" id=":id"><span class="fab fa-twitter"></span></a></li>',
-    'gplus' => '<li><a href=":url" class="social-button :class" id=":id"><span class="fab fa-google-plus-g"></span></a></li>',
-    'linkedin' => '<li><a href=":url" class="social-button :class" id=":id"><span class="fab fa-linkedin"></span></a></li>',
-    'whatsapp' => '<li><a target="_blank" href=":url" class="social-button :class" id=":id"><span class="fab fa-whatsapp"></span></a></li>',
+    'facebook' => '<a href=":url" class="social-button :class" id=":id"></a>',
+    'twitter' => '<a href=":url" class="social-button :class" id=":id"></a>',
+    'gplus' => '<a href=":url" class="social-button :class" id=":id"></a>',
+    'linkedin' => '<a href=":url" class="social-button :class" id=":id"></a>',
+    'whatsapp' => '<a target="_blank" href=":url" class="social-button :class" id=":id"></a>',
 ];
  
