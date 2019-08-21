7513ab6fe6d1e1911aea1c6a7d173c11775d1bac
Sander van Kasteel <info@sandervankasteel.nl>
2019-08-21 10:59:00 +0200

    Removed GooglePlus template URL

--- resources/lang/en/laravel-share-fa5.php
+++ resources/lang/en/laravel-share-fa5.php
@@ -3,7 +3,6 @@
 return [
     'facebook' => '<li><a href=":url" class="social-button :class" id=":id"><span class="fab fa-facebook-square"></span></a></li>',
     'twitter' => '<li><a href=":url" class="social-button :class" id=":id"><span class="fab fa-twitter"></span></a></li>',
-    'gplus' => '<li><a href=":url" class="social-button :class" id=":id"><span class="fab fa-google-plus-g"></span></a></li>',
     'linkedin' => '<li><a href=":url" class="social-button :class" id=":id"><span class="fab fa-linkedin"></span></a></li>',
     'whatsapp' => '<li><a target="_blank" href=":url" class="social-button :class" id=":id"><span class="fab fa-whatsapp"></span></a></li>',
     'pinterest' => '<li><a href=":url" class="social-button :class" id=":id"><span class="fab fa-pinterest"></span></a></li>',
