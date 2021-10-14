af6c60e7fcda13313829e261e59f456c1af0d73f
Maurizio <maurizio.lepora@gmail.com>
2021-10-14 14:54:34 +0200

    refactor: add php doc-blocks on ShareFacade.php

--- src/ShareFacade.php
+++ src/ShareFacade.php
@@ -4,6 +4,18 @@ namespace Jorenvh\Share;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method page($url, ?string $title = null, array $options = [], ?string $prefix = null, ?string $suffix = null)
+ * @method currentPage($url, ?string $title = null, array $options = [], ?string $prefix = null, ?string $suffix = null)
+ * @method facebook()
+ * @method twitter()
+ * @method reddit()
+ * @method telegram()
+ * @method whatsapp()
+ * @method linkedin(string $summary = '')
+ * @method pinterest()
+ * @method array getRawLinks()
+ */
 class ShareFacade extends Facade
 {
     /**
